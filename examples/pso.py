df7f2a2fa0435f201e2a02caf68f614a45c3052e
Tom Janson <tom.janson@rwth-aachen.de>
2017-09-25 00:50:09 +0200

    Extract bounds in example

--- examples/pso.py
+++ examples/pso.py
@@ -8,18 +8,22 @@ def numpy_paraboloid(arg_vector, center=0.5):
     return ((arg_vector - center) ** 2).sum(axis=1)
 
 
+def n_dimensional_unit_bounds(num_dimensions):
+    """Returns bounds of the form [0, 1]^n."""
+    lower_bounds = list(0 for _ in range(num_dimensions))
+    upper_bounds = list(1 for _ in range(num_dimensions))
+    return lower_bounds, upper_bounds
+
+
 if __name__ == '__main__':
     # the paraboloid objective function used in this demo works for an
     # arbitrary number of dimensions
     # thus the dimensionality is only determined by the search space
     # feel free to try any number of dimensions
-    DIMENSIONS = 3
-    LOWER_BOUNDS = list(0 for _ in range(DIMENSIONS))
-    UPPER_BOUNDS = list(1 for _ in range(DIMENSIONS))
-    BOUNDS = (LOWER_BOUNDS, UPPER_BOUNDS)
+    bounds = n_dimensional_unit_bounds(3)
 
     optimizer = ParticleSwarmOptimizer(numpy_paraboloid,
-                                       BOUNDS,
+                                       bounds,
                                        obj_fct_is_vectorized=True,
                                        options={'num_particles': 20, 'max_iters': 25})
     optimizer.optimize()
